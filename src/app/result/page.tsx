cbb7d05850982a2d8b040f10b22b6fd2d5530d9e
ChoSeyoung <a01075511157@gmail.com>
2024-08-26 00:35:43 +0900

    냥냥

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -5,6 +5,8 @@ import {useEffect, useState} from "react";
 import {getLunarMonthAndDay} from "@/utils/date/convertToLunarDate";
 import {NameMeaning} from "@/app/types/name.interface";
 import {generateNameWithMeaning} from "@/utils/name/generateName";
+import Image from "next/image";
+import bgResult from "@/assets/bg_result.jpg";
 
 export default function ResultPage() {
     const searchParams = useSearchParams();
@@ -23,10 +25,13 @@ export default function ResultPage() {
     if (!result) return (<p>loading...</p>)
 
     return (
-        <div>
-            <p>{result.name}</p>
-            <p>{result.monthMeaning}</p>
-            <p>{result.dayMeaning}</p>
+        <div className="w-full h-full max-w-screen-sm bg-white shadow-lg rounded-lg">
+            <Image src={bgResult} alt="result" className="w-full h-auto"/>
+            <div className="absolute left-1/2 top-2/3 transform -translate-x-1/2 -translate-y-1/2">
+                <p className="text-center text-5xl mb-5">{result.name}</p>
+                <p className="w-48 text-center mb-5">{result.monthMeaning}</p>
+                <p className="w-48 text-center mb-5">{result.dayMeaning}</p>
+            </div>
         </div>
     );
 }
