dbf1539d835637d219a863a9bdab64d1c529456f
ChoSeyoung <a01075511157@gmail.com>
2024-08-26 11:44:47 +0900

    결과페이지 텍스트 위치 수정

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -23,12 +23,14 @@ function ResultContent() {
     if (!result) return (<p>loading...</p>);
 
     return (
-        <div className="w-full h-full max-w-screen-sm bg-white shadow-lg rounded-lg">
-            <Image src={bgResult} alt="result" className="w-full h-auto"/>
-            <div className="absolute left-1/2 top-2/3 transform -translate-x-1/2 -translate-y-1/2">
-                <p className="text-center text-5xl mb-5">{result.name}</p>
-                <p className="w-48 text-center mb-5">{result.monthMeaning}</p>
-                <p className="w-48 text-center mb-5">{result.dayMeaning}</p>
+        <div className="relative w-full h-full max-w-screen-sm bg-white shadow-lg rounded-lg">
+            <Image src={bgResult} alt="result" layout="responsive" className="w-full h-auto"/>
+            <div className="absolute left-[50%] top-[56%] transform -translate-x-1/2 -translate-y-1/2 text-center flex flex-col justify-center items-center">
+                <p className="text-[5vw] sm:text-[5vw] md:text-[5vw] lg:text-[3vw] mb-5">{result.name}</p>
+                <p className="w-[50%] text-center text-[2vw] sm:text-[2vw] md:text-[1.5vw] lg:text-[1.25vw] xl:text-[1vw] 2xl:text-[0.75vw]  mb-5">{result.monthMeaning}</p>
+                <p className="w-[50%] text-center text-[2vw] sm:text-[2vw] md:text-[1.5vw] lg:text-[1.25vw] xl:text-[1vw] 2xl:text-[0.75vw] mb-5">{result.dayMeaning}</p>
+            </div>
+            <div className="bg-[#F1E5C8] p-5 w-full">
             </div>
         </div>
     );
@@ -37,7 +39,8 @@ function ResultContent() {
 export default function ResultPage() {
     return (
         <Suspense fallback={<p>로딩 중...</p>}>
-            <ResultContent />
+            <ResultContent/>
         </Suspense>
     );
 }
+
